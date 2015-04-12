3f9aad255a73eb8c1fb98e1647c20789d02c1b4f
lh <qidanei624@163.com>
2015-04-12 23:27:58 +0800

    cuixoing 1

--- Hello.java
+++ Hello.java
@@ -2,7 +2,6 @@ public class Hello
 {
 	public static void main(String args[]){
 		System.out.println("Hello 1");
-		System.out.println("Hello 2002");
-		System.out.println("Hello 300");
+		System.out.println("Hello 崔雄");
 	}
 }
