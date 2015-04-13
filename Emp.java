b1e2c8d32e7ab42f0baaadeff95ebff4babcf4f7
lh <qidanei624@163.com>
2015-04-13 16:39:23 +0800

    sdf

--- Emp.java
+++ Emp.java
@@ -1,7 +1,1 @@
-System.out.println("Emp master2222");
-System.out.println("Emp 500");
-System.out.println("Emp 600");
-System.out.println("Emp lh111");
-System.out.println("Emp 8000");
-System.out.println("Emp 9000");
 System.out.println("Emp 1");
